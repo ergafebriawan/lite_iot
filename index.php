27922027d2f301780b7f470aa0d3a803e7ab41c2
ergafebriawan <ergafebriawan@gmail.com>
2020-08-19 18:41:30 +0700

    fixed coding

--- index.php
+++ index.php
@@ -6,38 +6,40 @@ session_start();
 if ($_SESSION['status'] != "login") {
   header("location:login.php");
 }
-$sqlOutput = mysqli_query($connect, "SELECT * FROM output");
-
-while ($row = mysqli_fetch_assoc($sqlOutput)) {
 ?>
-  <!doctype html>
-  <html lang="en">
+<!doctype html>
+<html lang="en">
 
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
-    <title>small IOT</title>
-  </head>
+  <title>small IOT</title>
+</head>
 
-  <body>
-    <center>
+<body>
+  <div class="container">
 
-      <div class="container">
-        <h2>small IOT</h2><a href="logout.php" class="btn btn-danger">Logout</a>
+    <!-- Title header -->
+    <div class="row">
+      <div class="col-12 col-md-5 text-center mt-1 mx-auto p-1">
+        <h2>small IOT</h2> <a href="logout.php" class="btn btn-danger">Logout</a>
       </div>
+    </div>
 
-      
-
-      <div class="container">
-
+    <!-- Panel Control Relay/Lamp -->
+    <?php
+    $sqlOutput = mysqli_query($connect, "SELECT * FROM output");
+    while ($row = mysqli_fetch_assoc($sqlOutput)) {
+    ?>
+      <div class="col-5 col-md-5 text-center mt-1 mx-auto p-4">
         <div class="card">
-          <div class="card-header">
-            Panel Control
+          <div class="card-header bg-success">
+            Panel Control <?= $row['name'] ?>
           </div>
           <div class="card-body">
             <h5 class="card-title">
@@ -51,18 +53,20 @@ while ($row = mysqli_fetch_assoc($sqlOutput)) {
               ?>
             </h5>
             </p>
-            <a href="action.php?id=1&state=1" class="btn btn-success">ON</a>
-            <a href="action.php?id=1&state=0" class="btn btn-danger">OFF</a>
+            <a href="control.php?id=1&state=1" class="btn btn-success">ON</a>
+            <a href="control.php?id=1&state=0" class="btn btn-danger">OFF</a>
           </div>
         </div>
-      <?php }
+      </div>
+    <?php }
     $sqlInput = mysqli_query($connect, "SELECT * FROM input");
     while ($row = mysqli_fetch_assoc($sqlInput)) {
-      ?>
-
+    ?>
+    <!-- PANEL MONITOR-->
+      <div class="col-12 col-md-5 text-center mt-1 mx-auto p-4">
         <div class="card">
-          <div class="card-header">
-            Panel Sensor
+          <div class="card-header bg-primary">
+            Panel Sensor DHT22
           </div>
           <div class="card-body">
             <h5 class="card-title">Suhu: <?php echo $row['sensor1']; ?>C</h5>
@@ -70,17 +74,12 @@ while ($row = mysqli_fetch_assoc($sqlOutput)) {
             </p>
           </div>
         </div>
-    </center>
-  <?php }
+      </div>
+    <?php }
 
     $url = $_SERVER['REQUEST_URI'];
     header("Refresh: 5; URL=$url"); ?>
 
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-  </body>
+</body>
 
-  </html>
+</html>
--- login.php
+++ login.php
@@ -21,21 +21,7 @@
         </div>
 
         <div class="row">
-  <div class="col-12 col-md-5 text-center mx-auto p-4">
-  <?php session_start(); 
-  if ($_SESSION['message'] != '') : ?>
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            <strong><?= $_SESSION['message']; ?></strong>!
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-    <?php endif ?>
-  </div>
-  </div>
-
-        <div class="row">
-            <div class="col-18 col-md-5 mx-auto mt-3">
+            <div class="col-12 col-md-5 mx-auto mt-3">
                 <div class="card" style="width: 30rem;">
                     <div class="card-body bg-success">
                         <form action="prosesLogin.php" method="POST">
@@ -54,12 +40,6 @@
             </div>
         </div>
     </div>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
 </html>
